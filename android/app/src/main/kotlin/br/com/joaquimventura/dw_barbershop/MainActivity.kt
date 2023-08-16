929c58aee5a254946e649857f47a3477d4e6dbfa
Joaquim Ventura <joaquimventuradasilva@gmail.com>
2023-08-16 12:00:34 -0300

    First Commit

---
+++ android/app/src/main/kotlin/br/com/joaquimventura/dw_barbershop/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.joaquimventura.dw_barbershop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/features/splash/splash_page.dart
@@ -0,0 +1,77 @@
+import 'package:dw_barbershop/src/core/ui/constants.dart';
+import 'package:dw_barbershop/src/features/splash/auth/login/login_page.dart';
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  var _scale = 10.0;
+  var _animationOpacityLogo = 0.0;
+
+  double get _logoAnimationWidth => 100 * _scale;
+  double get _logoAnimationHeigth => 120 * _scale;
+
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      setState(() {
+        _animationOpacityLogo = 1.0;
+        _scale = 1.0;
+      });
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: DecoratedBox(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage(
+                ImageConstants.backgroundChair,
+              ),
+              opacity: 0.2,
+              fit: BoxFit.cover),
+        ),
+        child: Center(
+          child: AnimatedOpacity(
+              duration: const Duration(seconds: 1),
+              curve: Curves.easeIn,
+              opacity: _animationOpacityLogo,
+              onEnd: () {
+                Navigator.of(context).pushAndRemoveUntil(
+                    PageRouteBuilder(
+                      settings: const RouteSettings(name: '/auth/login'),
+                      pageBuilder: (context, animation, secondaryAnimation) {
+                        return const LoginPage();
+                      },
+                      transitionsBuilder: (_, animation, __, child) {
+                        return FadeTransition(
+                          opacity: animation,
+                          child: child,
+                        );
+                      },
+                    ),
+                    (route) => false);
+              },
+              child: AnimatedContainer(
+                  duration: const Duration(seconds: 3),
+                  width: _logoAnimationWidth,
+                  height: _logoAnimationHeigth,
+                  curve: Curves.linearToEaseOut,
+                  child: Image.asset(
+                    ImageConstants.imageLogo,
+                    fit: BoxFit.cover,
+                  ))),
+        ),
+      ),
+    );
+  }
+}
